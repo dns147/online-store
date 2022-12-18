f68d8c5b43342d747296d7242c2d4912cc1835bf
github-dns147 <rarden@ya.ru>
2022-12-18 22:43:41 +0300

    feat: add header footer

--- src/components/components.ts
+++ src/components/components.ts
@@ -6,16 +6,21 @@ export const Header = {
       <header class="container header">
         <div class="logo-container">
           <div class="logo"></div>
-          <h1 class="logo-name"><a href="#main" class="cart">Online Store</a></h1>
+          <h1>
+            <a href="#main" class="logo-name">
+              <span class="name">Star Wars</span>Store
+            </a>
+          </h1>
         </div>
 
         <div class="cart-total-container">
-          <p class="cart-total">Cart total: <span class="total-price"></span></p>
+          <p class="cart-total">Cart total: <span class="total-price">0</span></p>
         </div>
 
         <div class="cart-container">
           <a href="#cart" class="cart">
-            <img src="https://i.ibb.co/Ypg3JLJ/cart2.png" width="36" alt="image" class="cart-image">
+            <img src="https://i.ibb.co/Ypg3JLJ/cart2.png" width="50" alt="image" class="cart-image">
+            <span class="count-buy">0</span>
           </a>
         </div>
       </header>
@@ -36,17 +41,20 @@ export const Content = {
 export const Footer = {
   render: () => {
     return `
-      <footer class="container footer">
+      <footer class="footer">
         <ul class="footer-list">
-          <li class="footer-item">
-            <span class="copy">©2023</span>
-          </li>
-          <li class="footer-item">
-            <a href="https://rs.school/" class="footer-link">The Rolling Scopes School</a>
-          </li>
-          <li class="footer-item">
+          <li class="footer-item item-github">
+            <img src="https://svgshare.com/i/og2.svg" width="70" alt="icon" class="github-link">
             <a href="https://github.com/dns147" class="footer-link">dns147</a>
             <a href="https://github.com/Tatiana-Shylovich" class="footer-link">Tatiana-Shylovich</a>
+          </li>
+          <li class="footer-item">
+            <span class="copy">2023</span>
+          </li>
+          <li class="footer-item">
+            <a href="https://rs.school/js/" target="_blank">
+              <img src="https://rs.school/images/rs_school_js.svg" width="70" alt="icon" class="rs-link">
+            </a>
           </li>
         </ul>
       </footer>
